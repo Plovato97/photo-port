7ab9c8a10e41cae953dd4077a832731582707914
paul lovato <lovato.paul97@yahoo.com>
2022-11-18 19:15:31 -0500

    tests

--- src/components/About/__test__/index.test.js
+++ src/components/About/__test__/index.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
-import About from '../';
+import About from '..';
 
 afterEach(cleanup);
 
@@ -11,7 +11,7 @@ describe('About component', ()=> {
     it('renders', ()=> {
         render(<About />);
     });
-    it('matches snapshot DOm node structure', () => {
+    it('matches snapshot DOM node structure', () => {
         // render About
         const { asFragment } = render(<About />);
         expect(asFragment()).toMatchSnapshot();
